7cd1c08a2b24737cb9af91c38c69835e5a6bca84
divyesh0102 <divyesh.badrakiya@technostacks.com>
2023-07-21 17:30:00 +0530

    Update business and user profile business name fix, rewards business id condition

--- api/business/business.controller.js
+++ api/business/business.controller.js
@@ -319,7 +319,7 @@ exports.UpdateBusinessDetail = async (req, res) => {
 					res.send(setRes(resCode.ResourceNotFound, false, "Business not found.", null))
 				} else {
 					businessModel.findOne({
-						where: { is_deleted: false, business_name: { [Op.eq]: data.business_name }, id: { [Op.ne]: data.id } }
+						where: { is_deleted: false, business_name: { [Op.eq]: data.business_name.trim() }, id: { [Op.ne]: data.id } }
 					}).then(async nameData => {
 						if(nameData != null){
 							res.send(setRes(resCode.BadRequest, false, "This business name is already associated with another account.!", null))
@@ -1049,7 +1049,7 @@ exports.CreateBusiness = async (req, res) => {
 			}
 
 			var nameData = await businessModel.findOne({
-				where: { is_deleted: false, business_name: { [Op.eq]: data.business_name } }
+				where: { is_deleted: false, business_name: { [Op.eq]: data.business_name.trim() } }
 			});
 
 			if (nameData != null) {
@@ -1714,9 +1714,9 @@ exports.updateUserDetils = async (req, res) => {
 				if (_.isEmpty(businessDetail)) {
 					res.send(setRes(resCode.ResourceNotFound, false, "Business not found.", null))
 				} else {
-
+					const businessName = data.business_name ? data.business_name.trim() : data.business_name;
 					await businessModel.findOne({
-						where: { is_deleted: false, business_name: { [Op.eq]: data.business_name }, id: { [Op.ne]: data.id } }
+						where: { is_deleted: false, business_name: { [Op.eq]: businessName }, id: { [Op.ne]: data.id } }
 					}).then(async nameData => {
 						if (nameData != null) {
 							validation =false;
