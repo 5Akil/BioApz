e34df271379f3d8ee82acaa67c8c82b56a7b03f6
divyesh0102 <divyesh.badrakiya@technostacks.com>
2023-08-17 20:27:29 +0530

    purchase reward and earned reward list api

--- api/reward-history/index.js
+++ api/reward-history/index.js
@@ -7,5 +7,6 @@ const { authorize } = require('../../helpers/authorize');
 router.post('/list', authorize([2]), controller.rewardHistoryList);
 router.post('/business/list', authorize([3]), controller.rewardHistoryBusinessList)
 router.post('/perfomance', authorize([3]), controller.rewardPerfomance);
+router.post('/purchasedReward', authorize([3]), controller.purchasedEarnedRewardList);
 
 module.exports = router;
