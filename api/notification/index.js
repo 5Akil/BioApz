a192106832dc8eb68a6654b16ac1a4dbaac288cf
reetu.patel <reetu.patel@technostacks.in>
2023-08-29 10:08:19 +0530

    notification changes

--- api/notification/index.js
+++ api/notification/index.js
@@ -4,7 +4,7 @@ const {verifyToken} = require('../../config/token');
 const {authorize} = require('../../helpers/authorize');
 var controller = require('./notification.controller');
 
-router.get('/list', verifyToken, authorize([2,3]), controller.list);
+router.post('/list', verifyToken, authorize([2,3]), controller.list);
 router.post('/markAsRead', verifyToken, authorize([2,3]), controller.markAsRead);
 router.post('/delete', verifyToken, authorize([2,3]), controller.delete);
 
