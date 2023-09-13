459fb1eb6b3c594f2b6081c67584e0cff46fee72
reetu.patel <reetu.patel@technostacks.in>
2023-09-13 14:57:00 +0530

    device token changes

--- api/device-token/device-token.controller.js
+++ api/device-token/device-token.controller.js
@@ -6,9 +6,11 @@ var Op = models.Op;
 
 exports.deviceToken = async (req, res) => {
 	try{
-		const data = req.body;
-		const authData = req.user;
-		const device = models.device_tokens;
+		var data = req.body;
+		var authData = req.user;
+		var device = models.device_tokens;
+		var device_version = (data.api_version == 1) ? 'production' : 'testing';
+		var device_data = (data.device_type == 1) ? 'ios' : 'android';
 		var authModel = (authData.role_id == 3 && authData.role_id != 2) ? models.business : models.user;
 		//var requiredFields = _.reject(['device_id','device_type','device_token'], (o) => { return _.has(data, o) })
 		//if (requiredFields == '') {
@@ -24,30 +26,31 @@ exports.deviceToken = async (req, res) => {
 				condition.where = {...condition.where,...{user_id:authData.id}}
 			}
 			const alreadyLoggedInUser = await device.findOne(condition);
-			//if(alreadyLoggedInUser){
-			//	alreadyLoggedInUser.update({device_token:data.device_token});
-			//	return res.send(setRes(resCode.OK, true, "Device Token Updated Successfully", alreadyLoggedInUser))
-			//}else{
-			//	const deviceData = {
-			//		device_id: data.device_id,
-			//		device_type: (data.device_type == 1) ? 'ios' : 'android',
-			//		device_token: data.device_token,
-			//		api_version: (data.api_version == 1) ? 'production' : 'testing',
-			//		os_version: (data.os_version == undefined || _.isEmpty(data.os_version)) ? null : data.os_version,
-			//		app_version: (data.app_version == undefined || _.isEmpty(data.app_version)) ? null : data.app_version,
-			//		device_name: (data.device_name == undefined || _.isEmpty(data.device_name)) ? null : data.device_name,
-			//		model_name: (data.model_name == undefined || _.isEmpty(data.model_name)) ? null : data.model_name,
-			//	}
-			//	if(role_id === 2){
-			//		deviceData.user_id = id;
-			//	}else{
-			//		deviceData.business_id = id;
-			//	}
-			//	const storedevicetToken = await device.create(deviceData)
-			//	if(storedevicetToken){
-			//		return res.send(setRes(resCode.OK, true, "Device Registered Successfully", storedevicetToken))
-			//	}
-			//}
+			if(alreadyLoggedInUser){
+				alreadyLoggedInUser.update(data);
+				return res.send(setRes(resCode.OK, true, "Device Token Updated Successfully", alreadyLoggedInUser))
+			}
+			else{
+				const deviceData = {
+					device_id: data.device_id,
+					device_type: device_data,
+					device_token: data.device_token,
+					api_version: device_version,
+					os_version: (data.os_version == undefined || _.isEmpty(data.os_version)) ? null : data.os_version,
+					app_version: (data.app_version == undefined || _.isEmpty(data.app_version)) ? null : data.app_version,
+					device_name: (data.device_name == undefined || _.isEmpty(data.device_name)) ? null : data.device_name,
+					model_name: (data.model_name == undefined || _.isEmpty(data.model_name)) ? null : data.model_name,
+				}
+				if(authData.role_id === 2){
+					deviceData.user_id = authData.id;
+				}else{
+					deviceData.business_id = authData.id;
+				}
+				const storedevicetToken = await device.create(deviceData)
+				if(storedevicetToken){
+					return res.send(setRes(resCode.OK, true, "Device Registered Successfully", storedevicetToken))
+				}
+			}
 		//} else {
 		//	res.send(setRes(resCode.BadRequest, false, (requiredFields.toString() + ' are required'), null))
 		//}
