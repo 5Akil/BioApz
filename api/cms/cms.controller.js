f8e88315ebd6b398e44b3e621efd6aa02066979b
reetu.patel <reetu.patel@technostacks.in>
2023-09-18 16:58:23 +0530

    product related apis changes

--- api/cms/cms.controller.js
+++ api/cms/cms.controller.js
@@ -19,14 +19,14 @@ var fs = require('fs');
 var awsConfig = require('../../config/aws_S3_config');
 const {auth} = require('firebase-admin')
 
-exports.createCMS = async (req, res) => {
-	try{
+exports.createCMS = async (req,res) => {
+	try {
 		var data = req.body
 		var cmsModel = models.cms_pages
 		const businessModel = models.business;
 		const authUser = req.user;
 
-		var requiredFields = _.reject(['page_key', 'page_label', 'page_value'], (o) => { return _.has(data, o)  })
+		var requiredFields = _.reject(['page_key','page_label','page_value'],(o) => {return _.has(data,o)})
 
 		//const possiblePageKey = ['about','store_info','terms_of_service', 'how_to_use'];
 
@@ -34,135 +34,134 @@ exports.createCMS = async (req, res) => {
 		//	return res.send(setRes(resCode.BadRequest, false, `Possible value for page_key is one from ${possiblePageKey.join(',')}`,null));
 		//}
 
-		if(requiredFields == ""){
-			const blankValue = _.reject(['page_key', 'page_label', 'page_value'], (o) => { return data[o]  })
-			if (blankValue != "") {
-				return res.send(setRes(resCode.BadRequest, false, (blankValue.toString() + ' can not be blank'),null))
+		if(requiredFields == "") {
+			const blankValue = _.reject(['page_key','page_label','page_value'],(o) => {return data[o]})
+			if(blankValue != "") {
+				return res.send(setRes(resCode.BadRequest,false,(blankValue.toString() + ' can not be blank'),null))
 			}
 			await businessModel.findOne({
-				where: {id: authUser.id,is_deleted: false,is_active:true}
+				where: {id: authUser.id,is_deleted: false,is_active: true}
 			}).then(async business => {
-				if(business){
+				if(business) {
 					await cmsModel.findOne({
 						where:
 						{
-							page_key:data.page_key,
-							business_id:authUser.id,
-							is_deleted:false
+							page_key: data.page_key,
+							business_id: authUser.id,
+							is_deleted: false
 						}
 					}).then(async pageDetail => {
-						if(pageDetail != null){
-							res.send(setRes(resCode.BadRequest,false,"This page data already exist",null))
-						}else{
+						if(pageDetail != null) {
+							return res.send(setRes(resCode.BadRequest,false,"This page data already exist",null))
+						} else {
 							data.type = 'business';
 							data.business_id = authUser.id;
 							data.page_key = data.page_key.toLowerCase();
 							await cmsModel.create(data).then(async cmsData => {
-								res.send(setRes(resCode.OK,true,`${cmsData.page_key} page added successfully`,cmsData))
+								return res.send(setRes(resCode.OK,true,'Cms page added successfully',cmsData))
 							}).catch(error => {
-								console.log(error)
-								res.send(setRes(resCode.InternalServer,false,"Fail to add cms data",null))
+								return res.send(setRes(resCode.InternalServer,false,"Fail to add cms data",null))
 							})
 						}
 					})
-				}else{
-					res.send(setRes(resCode.ResourceNotFound, false, "Business not found.", null))
+				} else {
+					return res.send(setRes(resCode.ResourceNotFound,false,"Business not found.",null))
 				}
 			})
 
-		}else{
-			res.send(setRes(resCode.BadRequest, false, (requiredFields.toString() + ' are required'),null))
+		} else {
+			return res.send(setRes(resCode.BadRequest,false,(requiredFields.toString() + ' are required'),null))
 		}
-	} catch (error) {
-		res.send(setRes(resCode.BadRequest, false, "Something went wrong!", null))
+	} catch(error) {
+		return res.send(setRes(resCode.BadRequest,false,"Something went wrong!",null))
 	}
 }
 
-exports.viewCMS = async (req , res) => {
-	try{
+exports.viewCMS = async (req,res) => {
+	try {
 		var data = req.body
 		//var page_keys = data.page_key.split(',');
 		var Op = models.Op;
 		var cmsModel = models.cms_pages;
 		var type = (data.type == 0) ? 'admin' : 'business';
 
-		var requiredFields = _.reject(['type'], (o) => { return _.has(data, o)  })
+		var requiredFields = _.reject(['type'],(o) => {return _.has(data,o)})
 
 
 		let arrayFields = ['type'];
-		const result =  (data.type == 1) ? (arrayFields.push('business_id')) : '';
+		const result = (data.type == 1) ? (arrayFields.push('business_id')) : '';
 
-		var requiredFields = _.reject(arrayFields, (o) => { return _.has(data, o)  });
+		var requiredFields = _.reject(arrayFields,(o) => {return _.has(data,o)});
 		//const possiblePageKey = [];
 		//if(data.type == 0){
 		//	possiblePageKey.push('about','privacy_policy','terms_of_service');
 		//}else{
 		//	possiblePageKey.push('about','store_info','terms_of_service','how_to_use');
 		//}
-		
+
 		//if (data?.page_key && page_keys.map((k)=> !possiblePageKey.includes(k)).filter((t)=> t === true ).length > 0 ) {
 		//	return res.send(setRes(res.BadRequest, false, `Possible value for page_key are ${possiblePageKey.join(',')} `,null));
 		//}
-		if(requiredFields == ""){
+		if(requiredFields == "") {
 			var condition = {
-				attributes: { exclude: ['createdAt','updatedAt'] }
+				attributes: {exclude: ['createdAt','updatedAt']}
 			}
 			condition.where = {
 				//page_key:{
 				//	[Op.in]:page_keys
 				//},
 				//id:data.id,
-				is_enable:true,
-				is_deleted:false,
+				is_enable: true,
+				is_deleted: false,
 			}
-			if(type == 'admin'){
-				condition.where = {...condition.where,...{business_id:null}}
+			if(type == 'admin') {
+				condition.where = {...condition.where,...{business_id: null}}
 			}
-			if(!_.isEmpty(data.type)){
-				condition.where = {...condition.where,...{type:type}}
+			if(!_.isEmpty(data.type)) {
+				condition.where = {...condition.where,...{type: type}}
 			}
-			if(!_.isEmpty(data.business_id)){
-				condition.where = {...condition.where,...{business_id:data.business_id}}
+			if(!_.isEmpty(data.business_id)) {
+				condition.where = {...condition.where,...{business_id: data.business_id}}
 			}
-			if(!_.isEmpty(data.page_key)){
-				condition.where = {...condition.where,...{page_key:data.page_key}}
+			if(!_.isEmpty(data.page_key)) {
+				condition.where = {...condition.where,...{page_key: data.page_key}}
 			}
-			if(!_.isEmpty(data.id)){
-				condition.where = {...condition.where,...{id:data.id}}
+			if(!_.isEmpty(data.id)) {
+				condition.where = {...condition.where,...{id: data.id}}
 			}
 			await cmsModel.findAll(condition).then(async pageData => {
-				if(pageData != null){
-					for(const data of pageData){
-						if(type == 'admin'){
+				if(pageData != null) {
+					for(const data of pageData) {
+						if(type == 'admin') {
 							data.dataValues.page_url = commonConfig.admin_url + data.page_key;
 						}
 					}
-					if(pageData.length == 0){
+					if(pageData.length == 0) {
 						return res.send(setRes(resCode.ResourceNotFound,true,`Cms page Not Found`,null))
-					}else{
+					} else {
 						return res.send(setRes(resCode.OK,true,`Cms page get successfully`,pageData))
 					}
-				}else{
+				} else {
 					return res.send(setRes(resCode.ResourceNotFound,false,'Cms page not found',null))
 				}
 			}).catch(error => {
 				return res.send(setRes(resCode.BadRequest,false,"Fail to get page details",true))
 			})
-		}else{
-			return res.send(setRes(resCode.BadRequest, false, (requiredFields.toString() + ' are required'),null))
+		} else {
+			return res.send(setRes(resCode.BadRequest,false,(requiredFields.toString() + ' are required'),null))
 		}
-	} catch (error) {
-		return res.send(setRes(resCode.BadRequest, false, "Something went wrong!", null))
+	} catch(error) {
+		return res.send(setRes(resCode.BadRequest,false,"Something went wrong!",null))
 	}
 }
 
-exports.updateCMS = async (req, res) => {
-	try{
+exports.updateCMS = async (req,res) => {
+	try {
 		var data = req.body
 		var cmsModel = models.cms_pages
 
-		var requiredFields = _.reject(['id','page_key','page_label', 'page_value'], (o) => { return _.has(data, o)  })
-		
+		var requiredFields = _.reject(['id','page_key','page_label','page_value'],(o) => {return _.has(data,o)})
+
 		//const possiblePageKey = ['about','store_info','terms_of_service', 'how_to_use'];
 
 		//if (!data?.page_key || !possiblePageKey.includes(data?.page_key)) {
@@ -170,54 +169,54 @@ exports.updateCMS = async (req, res) => {
 		//}
 
 		if(requiredFields == "") {
-			const blankValue = _.reject(['id','page_key', 'page_label', 'page_value'], (o) => { return data[o]  })
-			if (blankValue != "") {
-				return res.send(setRes(resCode.BadRequest, false, (blankValue.toString() + ' can not be blank'),null))
+			const blankValue = _.reject(['id','page_key','page_label','page_value'],(o) => {return data[o]})
+			if(blankValue != "") {
+				return res.send(setRes(resCode.BadRequest,false,(blankValue.toString() + ' can not be blank'),null))
 			}
 			await cmsModel.findOne({
-				where:{
-					id:data.id,
-					page_key:data.page_key,
-					is_deleted:false,
-					is_enable:true
+				where: {
+					id: data.id,
+					page_key: data.page_key,
+					is_deleted: false,
+					is_enable: true
 				}
 			}).then(async pageData => {
 
-				if(pageData != null ){
-					if(data.page_key){
+				if(pageData != null) {
+					if(data.page_key) {
 						data.page_key = data.page_key.toLowerCase();
 					}
 					await cmsModel.update(data,{
-						where:{
-							id:data.id,
-							page_key:data.page_key
+						where: {
+							id: data.id,
+							page_key: data.page_key
 						}
 					}).then(async updateData => {
 
 						if(updateData == 1) {
-							
+
 							await cmsModel.findOne({
-								where:{
-									id:data.id,
-									page_key:data.page_key
+								where: {
+									id: data.id,
+									page_key: data.page_key
 								}
 							}).then(async pageDetails => {
-								res.send(setRes(resCode.OK,true,`${pageDetails.page_key} page updated successfully`,pageDetails))
+								return res.send(setRes(resCode.OK,true,'Cms page updated successfully',pageDetails))
 							})
-						}else{
-							res.send(setRes(resCode.InternalServer,false,'Internal server error',null))
+						} else {
+							return res.send(setRes(resCode.InternalServer,false,'Internal server error',null))
 						}
 					})
-				}else{
-					res.send(setRes(resCode.ResourceNotFound,false,"Page not found",null))
+				} else {
+					return res.send(setRes(resCode.ResourceNotFound,false,"Page not found",null))
 				}
 			}).catch(error => {
-				res.send(setRes(resCode.InternalServer,false,"Internal server error",null))
+				return res.send(setRes(resCode.InternalServer,false,"Internal server error",null))
 			})
-		}else{
-			res.send(setRes(resCode.BadRequest, false, (requiredFields.toString() + ' are required'),null))	
+		} else {
+			return res.send(setRes(resCode.BadRequest,false,(requiredFields.toString() + ' are required'),null))
 		}
-	} catch (error) {
-		res.send(setRes(resCode.BadRequest, false, "Something went wrong!", null))
+	} catch(error) {
+		return res.send(setRes(resCode.BadRequest,false,"Something went wrong!",null))
 	}
 }
