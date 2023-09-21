cb584eb2df63d031e734531816cae23eb160cbb4
reetu.patel <reetu.patel@technostacks.in>
2023-09-21 16:44:18 +0530

    update

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ var NotificationQueue = new Queue(async function(task,cb) {
 					NotificationData.device_token = deviceToken?.device_token;
 					NotificationData.message = task.text
 					NotificationData.title = task.from_name
-					NotificationData.content = {notification_type: NOTIFICATION_TYPES.CHAT_NOTIFICATION,user_id: user.id,business_id: task?.receiver_id,from_name: task?.from_name}
+					NotificationData.content = {notification_type: NOTIFICATION_TYPES.CHAT_NOTIFICATION,user_id: user.id,business_id: task?.business_id}
 					notification.SendNotification(NotificationData)
 				}
 			}
@@ -149,7 +149,7 @@ var NotificationQueue = new Queue(async function(task,cb) {
 					NotificationData.device_token = deviceToken?.device_token
 					NotificationData.message = task.text
 					NotificationData.title = task.from_name
-					NotificationData.content = {notification_type: NOTIFICATION_TYPES.CHAT_NOTIFICATION,user_id: task?.sender_id,business_id: business?.id,from_name: task?.from_name}
+					NotificationData.content = {notification_type: NOTIFICATION_TYPES.CHAT_NOTIFICATION,user_id: task?.user_id,business_id: business?.id}
 					notification.SendNotification(NotificationData)
 				}
 			}
